f685d88c754164f6295d35c96febf3799129ceb7
DongHwan <DongHwan@LAPTOP-G1H40BPA>
2019-04-08 12:53:38 +0900

    recommit

--- WhatTheHealth/src/test/java/com/wthealth/service/meeting/test/MeetingServiceTest.java
+++ WhatTheHealth/src/test/java/com/wthealth/service/meeting/test/MeetingServiceTest.java
@@ -20,14 +20,7 @@ import com.wthealth.domain.Reply;
 import com.wthealth.service.meeting.MeetingService;
 
 
-/*
- *	FileName :  UserServiceTest.java
- * �� JUnit4 (Test Framework) �� Spring Framework ���� Test( Unit Test)
- * �� Spring �� JUnit 4�� ���� ���� Ŭ������ ���� ������ ��� ���� �׽�Ʈ �ڵ带 �ۼ� �� �� �ִ�.
- * �� @RunWith : Meta-data �� ���� wiring(����,DI) �� ��ü ����ü ����
- * �� @ContextConfiguration : Meta-data location ����
- * �� @Test : �׽�Ʈ ���� �ҽ� ����
- */
+ 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration	(locations = {	"classpath:config/context-common.xml",
 		"classpath:config/context-aspect.xml",
@@ -35,7 +28,6 @@ import com.wthealth.service.meeting.MeetingService;
 		"classpath:config/context-transaction.xml" })
 public class MeetingServiceTest {
 
-	//==>@RunWith,@ContextConfiguration �̿� Wiring, Test �� instance DI
 	@Autowired
 	@Qualifier("meetingServiceImpl")
 	private MeetingService meetingService;
@@ -48,17 +40,17 @@ public class MeetingServiceTest {
 		
 		meeting.setCheifId("horannabi");
 		meeting.setDepoCondition("0");
-	//meeting.setMeetTime(date);
+	
 
-		System.out.println("�������� �ñ��� :: "+meeting.getMeetNo());
+		
 		meetingService.addMeeting(meeting);
-		System.out.println("����� �ñ��� :: "+meeting.getMeetNo());
+		
 		meeting = meetingService.getMeeting(10026);
 		
-		//==> console Ȯ��
+		//==> console 확占쏙옙
 		System.out.println(meeting);
 		
-		//==> API Ȯ��
+		//==> API 확占쏙옙
 		Assert.assertEquals("user3", meeting.getCheifId());
 		Assert.assertEquals("0", meeting.getDepoCondition());
 		Assert.assertEquals("1970-01-01", meeting.getMeetTime().toString());
@@ -76,51 +68,19 @@ public class MeetingServiceTest {
 		
 		Post post = new Post();
 		post.setUserId("horannabi");
-		post.setTitle("daoimpl���� �Ѵ� �ֱ�~");
-		post.setContents("�ɰž�!");
+		post.setTitle("daoimpl");
+		post.setContents("占daoimpl!");
 		
 		meeting.setPost(post);
 		System.out.println(meeting);
 		
 		meetingService.addMeeting(meeting);
-		/*post.setPostNo("ME"+meeting.getMeetNo());
-		System.out.println(" postNo�� ������?:"+post);
-		meetingService.addMeetingPost(post);*/
+
 		Assert.assertEquals("horannabi", meeting.getPost().getUserId());
 		Assert.assertEquals("0", meeting.getDepoCondition());
 		Assert.assertEquals("70-01-01", meeting.getMeetTime().toString());
 	}
 	
-	//@Test
-	/*public void testAddMeetingPost() throws Exception {
-		Date date = new Date(20190101);
-		
-		Meeting meeting = new Meeting();
-		Post post = new Post();
-		post.setUserId("user3");
-		post.setTitle("��϶� �Ҹ��� ����~~~");
-		post.setContents("��¥ ��վ��~~~~~~");
-		
-		meeting.setPost(post);
-		meeting.setCheifId("user3");
-		meeting.setDepoCondition("0");
-		meeting.setMeetTime(date);
-
-		System.out.println("�������� �ñ��� :: "+meeting.getMeetNo());
-		meetingService.addMeeting(meeting);
-		post.setPostNo("ME"+meeting.getMeetNo());
-		System.out.println("����� �ñ��� :: "+meeting.getMeetNo());
-		meetingService.addMeetingPost(post);
-		meeting = meetingService.getMeeting("ME10034");
-		
-		//==> console Ȯ��
-		System.out.println(meeting);
-		
-		//==> API Ȯ��
-		Assert.assertEquals("user3", meeting.getCheifId());
-		Assert.assertEquals("0", meeting.getDepoCondition());
-		Assert.assertEquals("1970-01-01", meeting.getMeetTime().toString());
-	}*/
 	
 	//@Test
 	public void testGetMeeting() throws Exception {
@@ -155,10 +115,10 @@ public class MeetingServiceTest {
 		meetingService.addJoin(join);
 		join = meetingService.getJoin(join.getJoinNo());
 		
-		//==> console Ȯ��
+		//==> console 확占쏙옙
 		System.out.println(join);
 		
-		//==> API Ȯ��
+		//==> API 확占쏙옙
 		Assert.assertEquals("user1", join.getPartyId());
 		Assert.assertEquals("0", join.getDepoStatus());
 	}
@@ -193,11 +153,11 @@ public class MeetingServiceTest {
 	 	List<Object> list = (List<Object>)map.get("list");
 	 	Assert.assertEquals(3, list.size());
 	 	
-	 	//==> console Ȯ��
+	 	//==> console 확占쏙옙
 	 	System.out.println(list);
 	 	
 	 	Integer totalCount = (Integer)map.get("totalCount");
-	 	System.out.println("product list all ��Żī��� : "+totalCount);
+	 	System.out.println("product list all 占쏙옙탈카占쏙옙占� : "+totalCount);
 	 	
 	 	System.out.println("=======================================");
 	 	
@@ -211,11 +171,11 @@ public class MeetingServiceTest {
 	 	list = (List<Object>)map.get("list");
 	 	Assert.assertEquals(3, list.size());
 	 	
-	 	//==> console Ȯ��
-	 	System.out.println("product list all ����Ʈ : "+list);
+	 	//==> console 확占쏙옙
+	 	System.out.println("product list all 占쏙옙占쏙옙트 : "+list);
 	 	
 	 	totalCount = (Integer)map.get("totalCount");
-	 	System.out.println("product list all 3�� �� ����Ʈ ���� : "+totalCount);
+	 	System.out.println("product list all 3占쏙옙 占쏙옙 占쏙옙占쏙옙트 占쏙옙占쏙옙 : "+totalCount);
 	}
 	
 	//@Test
@@ -228,11 +188,11 @@ public class MeetingServiceTest {
 	 	List<Object> list = (List<Object>)map.get("list");
 	 	Assert.assertEquals(3, list.size());
 	 	
-	 	//==> console Ȯ��
+	 	//==> console 확占쏙옙
 	 	System.out.println(list);
 	 	
 	 	Integer totalCount = (Integer)map.get("totalCount");
-	 	System.out.println("product list all ��Żī��� : "+totalCount);
+	 	System.out.println("product list all 占쏙옙탈카占쏙옙占� : "+totalCount);
 	 	
 	 	System.out.println("=======================================");
 	 	
@@ -244,11 +204,11 @@ public class MeetingServiceTest {
 	 	list = (List<Object>)map.get("list");
 	 	Assert.assertEquals(3, list.size());
 	 	
-	 	//==> console Ȯ��
-	 	System.out.println("product list all ����Ʈ : "+list);
+	 	//==> console 확占쏙옙
+	 	System.out.println("product list all 占쏙옙占쏙옙트 : "+list);
 	 	
 	 	totalCount = (Integer)map.get("totalCount");
-	 	System.out.println("product list all 3�� �� ����Ʈ ���� : "+totalCount);
+	 	System.out.println("product list all 3占쏙옙 占쏙옙 占쏙옙占쏙옙트 占쏙옙占쏙옙 : "+totalCount);
 	}
 	 
 }
